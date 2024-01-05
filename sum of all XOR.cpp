301bb5d007d495441f04aae5ce0a7932f29099fa
Deepak babu P <99535087+deepakdeepu000@users.noreply.github.com>
2024-01-05 21:41:20 +0530

    Update sum of all XOR.cpp

--- sum of all XOR.cpp
+++ sum of all XOR.cpp
@@ -6,7 +6,8 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
-    int a;
+    // brute force approch
+    /*int a;
     cin >> a;
     int arr[a];
     for(int i = 0;i < a;i++){
@@ -17,5 +18,24 @@ int main()
             sum += arr[i]^arr[j];     
     }
     }
-    cout << sum ;
+    cout << sum ;*/
+
+    // sum of xor of array elments using bit manipulation
+    long res = 0; // Initialize the res variable
+
+    for (int i = 0; i < 32; i++) {
+        long zero_count = 0, one_count = 0; // Initialize counts of zeros and ones for each bit position
+        for (int j = 0; j < n; j++) {
+            // Checking if the i-th bit is set in arr[j]
+            if ((arr[j] & (1 << i)) != 0) {
+                one_count++; // Increment the count of set bits
+            } else {
+                zero_count++; // Increment the count of unset bits
+            }
+        }
+        
+        // Add the contribution of the current bit position to the final result
+        res += (one_count*zero_count*pow(2, i));
+    }
+    cout << res; // Output the result
 }
