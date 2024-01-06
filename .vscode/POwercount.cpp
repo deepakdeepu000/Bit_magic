e6970dfe3730e9e17867b9c41df75dc861b42576
Deepak babu <21mh1a4242@acoe.edu.in>
2024-01-06 22:40:40 +0530

    prime power count Using sieve

--- .vscode/POwercount.cpp
+++ .vscode/POwercount.cpp
@@ -3,49 +3,56 @@
 
 using namespace std;
 
-vector<int> sieve_arr(int n) {
-    vector<int> sieve(n + 1, 0);
+vector<int> sieve_arr(int n){
+        
+        vector<int> sieve(n+1);
+        
+        for(int i = 2; i <= n;i++){
+	        sieve[i] = i;
+	        // cout << sieve[i] << " ";
+	    }
+	    
+        for(int i = 2;i*i <= n;i++){
+	        if(sieve[i]%i == 0){
+	            for(int j = i*i; j <= n;j+=i){
+	                sieve[j] = i;
+	            }
+	        }
+	    }
+	    
+	    return sieve;
+        
+    }
 
-    for (int i = 2; i * i <= n; i++) {
-        if (sieve[i] == 0) {
-            for (int j = i * i; j <= n; j += i) {
-                if (sieve[j] == 0) {
-                    sieve[j] = i;
-                }
-            }
+   
+    int degreeSum(vector<int> &sieve,int num){
+        
+        if(num <= 1){
+            return 0;
         }
+        int c = 0;
+        if(num%sieve[num] == 0){
+            c+=1;
+        }
+        return c+degreeSum(sieve,num/sieve[num]);  
+        
     }
-
-    return sieve;
-}
-
-int degreeSum(vector<int> &sieve, int num) {
-    if (num == 1) {
-        return 0;
-    }
-
-    int primeFactor = sieve[num];
-    int count = 0;
-
-    // Count the power of the prime factor in the number
-    while (num % primeFactor == 0) {
-        num /= primeFactor;
-        count++;
-    }
-
-    return count + degreeSum(sieve, num);
-}
-
-int sumOfPowers(int a, int b) {
-    int sum = 0;
-    vector<int> sieve = sieve_arr(b);
-
-    for (int i = a; i <= b; i++) {
-        sum += degreeSum(sieve, i);
-    }
-
-    return sum;
-}
+    
+    
+	int sumOfPowers(int a, int b){
+	    // Code here
+	    int sum = 0;
+	    
+	    vector<int> sieve = sieve_arr(b);
+	    
+	    for(int i = a;i <= b;i++){
+	       //cout << sieve[i] << " ";
+	       sum += degreeSum(sieve,i);
+	    }
+	    
+	    return sum;
+	    
+	}
 
 int main() {
     // Example 1
