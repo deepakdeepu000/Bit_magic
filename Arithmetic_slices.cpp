b8731af2dfd59f2e0a12f6f791dcba356d0e9175
Deepak babu P <99535087+deepakdeepu000@users.noreply.github.com>
2024-01-07 23:08:20 +0530

    Create Arithmetic_slices.cpp

---
+++ Arithmetic_slices.cpp
@@ -0,0 +1,18 @@
+int n = nums.size();
+        int cnt = 0;
+        if(n < 3){
+            return 0;
+        }
+        for(int i = 0;i < n-2;i++){
+            int diff = nums[i+1]-nums[i];
+            for(int j = i+2;j < n;j++){
+                int diff2 = nums[j]-nums[j-1];
+                if(diff == diff2){
+                    cnt++;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        return cnt;
